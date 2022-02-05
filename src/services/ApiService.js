d3d2c2a5e0d00bbf62323f85c9ad605c8fc100a9
Liza <gliza.gavrilova@yandex.ru>
2022-02-05 19:49:28 +0300

    Error

--- src/services/ApiService.js
+++ src/services/ApiService.js
@@ -1,17 +1,21 @@
 export default class ApiService {
-  _apiBase = 'https://api.themoviedb.org/3/';
+  apiBase = 'https://api.themoviedb.org/3/';
+
   apiKey = '951df45a72c2a2a67182d7d2628cf7d4';
 
   async getResource(url) {
-    const res = await fetch(`${this._apiBase}${url}`);
+    const res = await fetch(`${this.apiBase}${url}`);
     if (!res.ok) {
       throw new Error(`Could not fetch ${url}, received ${res.status}`);
     }
-    return await res.json();
-  };
+    const body = await res.json();
+    return body;
+  }
 
   async getAllMovies(searchQuery, pageNumber) {
-    return await this.getResource(`search/movie?api_key=${this.apiKey}&include_adult=false&query=${searchQuery}&page=${pageNumber}`);
-  
-  };
+    const res = await this.getResource(
+      `search/movie?api_key=${this.apiKey}&include_adult=false&query=${searchQuery}&page=${pageNumber}`
+    );
+    return res;
+  }
 }
